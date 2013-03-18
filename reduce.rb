8d5c9a5595e6997c73de0d1088e82e5e5c1cea3e
Be <bebirchall@gmail.com>
2013-03-18 10:21:34 -0400

    reduce, map, and filter

--- reduce.rb
+++ reduce.rb
@@ -1,41 +1,19 @@
-# reduce 
-
-# def my__other_reduce(proc, array, start)
-#   if array == []
-#   	start
-#   else proc.call(start, my__other_reduce(proc, array.drop(1), array.first))
-#   end
-# end
-
-# my_sum = Proc.new { |x, y| x + y }
-# answer = my_other_reduce(my_sum, [1,2,3], 0)
-# #puts answer
 
 class Array
 
 	def my_reduce(start, &block)
 		if self == []
 			start
-		else block.call(start, self.drop(1).my_reduce(self.first, &block))
+		else self.drop(1).my_reduce(yield(start, self.first), &block)
 		end
 	end
 
-end
+	def my_map
+		self.my_reduce([]) { |x, y| x << yield(y) }
+	end
 
-class Array
-
-	def my_map(&block)
-		
-
+	def my_filter
+		self.my_reduce([]) { |x, y| yield(y) ? x << y : x }
 	end
 
 end
-
-
-
-puts [1,2,3].my_reduce(0) { |x, y| x + y }
-
-
-# implement map using my_reduce
-
-# array.map { |x| x * 2}
