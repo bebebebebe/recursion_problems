c76a2a6d0f9c530725c77efe4e679c5578e89062
Be <bebirchall@gmail.com>
2013-03-18 10:23:25 -0400

    tabs

--- reduce.rb
+++ reduce.rb
@@ -1,19 +1,19 @@
 
 class Array
 
-	def my_reduce(start, &block)
-		if self == []
-			start
-		else self.drop(1).my_reduce(yield(start, self.first), &block)
-		end
-	end
+  def my_reduce(start, &block)
+    if self == []
+      start
+    else self.drop(1).my_reduce(yield(start, self.first), &block)
+    end
+  end
 
-	def my_map
-		self.my_reduce([]) { |x, y| x << yield(y) }
-	end
+  def my_map
+    self.my_reduce([]) { |x, y| x << yield(y) }
+  end
 
-	def my_filter
-		self.my_reduce([]) { |x, y| yield(y) ? x << y : x }
-	end
+  def my_filter
+    self.my_reduce([]) { |x, y| yield(y) ? x << y : x }
+  end
 
 end
